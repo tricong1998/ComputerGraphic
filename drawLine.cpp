33ffb0c25eb271e5def1b8b8c6b6a195093ed8d3
phamkhacan1903 <phamkhacan1903@gmail.com>
2018-11-09 11:04:23 +0700

    update_function

--- drawLine.cpp
+++ drawLine.cpp
@@ -1,28 +1,28 @@
-#include<iostream>
+
 #include<gl/glut.h>
 using namespace std;
-GLuint g_box;
 
-GLuint MakeCylinder(const float& radius, const float& length)
+GLuint g_box;
+void MakeCylinder(const float& radius, const float& length)
 {
 	glPushMatrix();
- GLuint dp_list;
- dp_list = glGenLists(1); 
- glNewList(dp_list, GL_COMPILE);
- GLUquadricObj *quadratic_obj;
- quadratic_obj = gluNewQuadric();
- 
- 
- glRotatef(-90, 1.0, 0.0, 0.0);
- gluCylinder(quadratic_obj, radius, radius, length, 32, 32);
- glPopMatrix();
-// glRotatef(-90, 1.0, 0.0, 0.0);
- glEndList();
- return dp_list;
+ 	//GLuint dp_list;
+// 	dp_list = glGenLists(1); 
+// 	glNewList(dp_list, GL_COMPILE);
+	GLUquadricObj *quadratic_obj;
+ 	quadratic_obj = gluNewQuadric();
+ 	glRotatef(-90, 1.0, 0.0, 0.0);
+ 	gluCylinder(quadratic_obj, radius, radius, length, 32, 32);
+ 	glPopMatrix();
+ 	//glEndList();
+// 	return dp_list;
 }
+
 void drawLine(float x) {
-//	MakeCylinder( radius,  length);
-	g_box = MakeCylinder(0.02,x);
+
+	MakeCylinder(0.02,x);
 	glCallList(g_box);
-	glTranslatef(0,x,0);
+//	glTranslatef(0,x,0);
 }
+
+
